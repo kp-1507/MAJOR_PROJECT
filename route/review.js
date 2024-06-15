a7b66d17be1bd4b37b31e75fe0782cd0d270ff95
kp-1507 <142100518+kp-1507@users.noreply.github.com>
2024-06-15 18:07:18 +0530

    Update review.js

--- route/review.js
+++ route/review.js
@@ -1,26 +1,17 @@
 const express = require("express");
 const router = express.Router({mergeParams: true}); 
 const wrapAsync=require("../utils/wrapAsync.js");
-const ExpressError=require("../utils/ExpressError.js");
-const {reviewSchema}=require("../schema.js");
+const {validateReview,isLoggedIn,isReviewAuthor}=require("../middleware.js");
 const Review=require("../models/review.js");
 const Listing=require("../models/listing.js");
+// const reviewController=require("../controller/reviews.js");
 
-const validateReview=(req,res,next)=>{
-    let {error}=reviewSchema.validate(req.body);
-    // console.log(result);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }
-    else {
-        next();
-    }
-};
   //REVIEWS POST ROUTE
-  router.post("/",validateReview ,wrapAsync(async(req,res)=>{
+  router.post("/",isLoggedIn,validateReview ,wrapAsync(async(req,res)=>{
     let listing=await Listing.findById(req.params.id);
     let newReview=new Review(req.body.review);
+    newReview.author=req.user._id;
+    console.log(newReview);
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
@@ -28,7 +19,7 @@ const validateReview=(req,res,next)=>{
     res.redirect(`/listings/${listing._id}`);
   }));
   //DELETE REVIEW ROUTE
-  router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+  router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async(req,res)=>{
     let {id,reviewId}=req.params;
     await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
     await Review.findByIdAndDelete(reviewId);
