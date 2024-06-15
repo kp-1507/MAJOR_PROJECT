0bf198bbb4a11673373c0445b0771a9bc35c0e6a
kp-1507 <142100518+kp-1507@users.noreply.github.com>
2024-06-15 18:01:56 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,13 +4,17 @@
 // const ExpressError = require("./utils/ExpressError.js");
 // const {listingSchema, reviewSchema} = require("./schema.js");
 
+const Listing=require("./models/listing");
+const Review=require("./models/review");
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema,reviewSchema}=require("./schema.js");
 
 module.exports.isLoggedIn = (req,res,next)=>{
   //  console.log(req);
    // console.log(req.path, "...", req.originalUrl);   
       
     if(!req.isAuthenticated()){          // isAuthenticate() is a method that checks info stored in session using passport//
-        req.session.redirectUrl = req.originalUrl;       // if user not logged in before we take there redirect url//
+        req.session.redirectUrl=req.originalUrl;      // if user not logged in before we take there redirect url//
         req.flash("error","you must be logged in!");
         return res.redirect("/login");
     }
@@ -20,14 +24,53 @@ module.exports.isLoggedIn = (req,res,next)=>{
 
 // so, we will use "locals" to save our "req.session.redirectUrl" redirectUrl value , As passport don't have access to delete locals//
 
-// module.exports.saveRedirectUrl = (req,res,next)=>{
-//     if(req.session.redirectUrl){
-//         res.locals.redirectUrl = req.session.redirectUrl;      // saving req's redirectUrl value to res's local redirectUrl variable//
-//     }
-//     next();
-// }
+module.exports.saveRedirectUrl = (req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;      // saving req's redirectUrl value to res's local redirectUrl variable//
+    }
+    next();
+}
+module.exports.isOwner=async(req,res,next)=>{
+    let { id } = req.params;
+     let listing=await Listing.findById(id);
+     if(!listing.owner._id.equals(res.locals.currUser._id)){
+        req.flash("error","You are not the owner of this listing");
+        return res.redirect(`/listings/${id}`);
+     }
+     next();
+}
 
-
+module.exports.validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    // console.log(result);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }
+    else {
+        next();
+    }
+}
+module.exports.isReviewAuthor=async(req,res,next)=>{
+    let {id, reviewId } = req.params;
+     let review=await Review.findById(reviewId);
+     if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error","You are not the owner of this review");
+        return res.redirect(`/listings/${id}`);
+     }
+     next();
+}
+module.exports.validateReview=(req,res,next)=>{
+    let {error}=reviewSchema.validate(req.body);
+    // console.log(result);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }
+    else {
+        next();
+    }
+};
 // module.exports.isOwner = async(req,res,next)=>{
 //     let {id} = req.params;
 //     let listing = await listings.findById(id);
