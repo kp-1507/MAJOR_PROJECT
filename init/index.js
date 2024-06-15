be27210c13c73db4f730a176a18f450bc6d3c940
kp-1507 <142100518+kp-1507@users.noreply.github.com>
2024-06-15 18:09:10 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data=initData.data.map((obj)=>({...obj,owner:"666d165c1053c79722b440b7"}))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
