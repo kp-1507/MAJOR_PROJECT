457bf2c57feeb58a04aeb6f8a007fd58f55159b0
kp-1507 <142100518+kp-1507@users.noreply.github.com>
2024-06-16 15:55:27 +0530

    Update listing.js

--- route/listing.js
+++ route/listing.js
@@ -1,26 +1,32 @@
 const express = require("express");
 const router = express.Router();           // creating our router object//
 const wrapAsync = require("../utils/wrapAsync.js");
-
 const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
 const listingController=require("../controller/listings.js");
+const multer  = require('multer');
+const {storage}=require("../cloudConfig.js");
+const upload = multer({ storage});
+ 
+router.route("/")
+.get( wrapAsync(listingController.index))
+.post(
+        isLoggedIn,
+        upload.single('listing[image]'),validateListing,
+        wrapAsync(listingController.createListing)
+    );
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+  //New Route
+  router.get("/new", isLoggedIn,listingController.renderNewForm);
+
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(isLoggedIn,upload.single('listing[image]'),isOwner,validateListing,wrapAsync(listingController.updateListing ))
+.delete(isLoggedIn, isOwner,wrapAsync(listingController.destroyListings));
 
  
-
- //INDEX ROUTE
-router.get("/", wrapAsync(listingController.index));
-  //New Route
- router.get("/new", isLoggedIn,listingController.renderNewForm);
- //SHOW ROUTE
- router.get("/:id",wrapAsync(listingController.showListing));
- //CREATE ROUTE
- router.post("/", isLoggedIn,validateListing,wrapAsync(listingController.createListing) );
  //EDIT ROUTE
  router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync( listingController.renderEditForm));
- //UPDATE ROUTE
- router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing ));
-   //Delete Route
- router.delete("/:id",isLoggedIn, isOwner,wrapAsync(listingController.destroyListings));
-   
+ 
 
 module.exports=router;
